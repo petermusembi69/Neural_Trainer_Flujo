266599bd4185e42128b698f926065a1981396666
petermusembi69 <nyongesamusembi@gmail.com>
2022-11-15 18:40:09 +0300

    Remove new line

--- lib/models/on_borarding_model.dart
+++ lib/models/on_borarding_model.dart
@@ -32,7 +32,7 @@ final onBoradingScreens = [
     backGroundImage: 'assets/images/conecta_con_cel.png',
     title: "CONECTA",
     subtitle:
-        '''Conecta tus neuro sensores a la aplicación\n Neural Trainer y comienza a aumentar tu\n rendimiento cognitivo.''',
+        '''Conecta tus neuro sensores a la aplicación Neural Trainer y comienza a aumentar tu rendimiento cognitivo.''',
     backGroundColor: $styles.colors.black,
   ),
   OnBoardingModel(
@@ -40,7 +40,7 @@ final onBoradingScreens = [
     backGroundImage: 'assets/images/entrena_con_cel.png',
     title: 'ENTRENA',
     subtitle:
-        '''Selecciona una actividad creada por el equipo\n de Neural Trainer o crea tu propio\n entrenamiento específico.''',
+        '''Selecciona una actividad creada por el equipo de Neural Trainer o crea tu propio entrenamiento específico.''',
     backGroundColor: $styles.colors.black,
   ),
   OnBoardingModel(
@@ -48,7 +48,7 @@ final onBoradingScreens = [
     backGroundImage: 'assets/images/analiza_con_cel.png',
     title: "ANALIZA",
     subtitle:
-        '''Monitorea el desempeño de tus atletas, registra\n sus resultados y compártelos en tiempo real.''',
+        '''Monitorea el desempeño de tus atletas, registra sus resultados y compártelos en tiempo real.''',
     backGroundColor: $styles.colors.black,
   ),
 ];
